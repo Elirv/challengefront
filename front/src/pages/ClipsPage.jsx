13c4a3506257e74be6621a60d615e27914783960
Elirv <elizabeth.ramos101@hotmail.com>
2023-02-08 16:28:51 +0000

    front

--- front/src/pages/ClipsPage.jsx
+++ front/src/pages/ClipsPage.jsx
@@ -4,8 +4,8 @@ import { NavbarContainer } from '../components/InNavbar';
 export const ClipsPage = () => {
   return (
     <>
+      <h1>gifs</h1>
       <NavbarContainer />
-      <div style="width:480px"><iframe allow="fullscreen"  height="853" src="https://giphy.com/embed/a1wBaMlA5TTCpCsrau/video" width="480"></iframe></div>
-    </>
+      </>
   )
 }
